9609511d4d8c3e2d48048453c75fbb43867526da
kbondar17 <ironbondar@gmail.com>
2022-10-31 11:40:53 +0300

    удалил ненужное

--- redis_client.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-
-class RedisCli:
-    def __init__(self) -> None:
-        self = redis.Redis(host='localhost', port=6379, db=0)
-
-redis_cli = redis.Redis(host='localhost', port=6379, db=0)
