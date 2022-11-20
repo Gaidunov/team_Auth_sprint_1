9b82dba8b820927e39e065ad023cac40505bc76b
kbondar17 <ironbondar@gmail.com>
2022-11-20 18:03:49 +0300

    fixed typo

--- src/api/v1/users.py
+++ src/api/v1/users.py
@@ -41,6 +41,7 @@ def vk_oauth():
     print('vk_settings.oath_url---', vk_settings.oath_url)
     return redirect(vk_settings.oath_url)
 
+
 @routes.get('account/vk/register')
 @catch_http_errors
 def vk_registration():
@@ -55,6 +56,7 @@ def vk_registration():
 
     temp_password = db_manager.users.register_via_vk(vk_data.email)
     return f'зарегались. временный пароль {temp_password}. поменяй при первой возможности'
+
 
 @routes.post('account/change_password')
 @jwt_required()
@@ -76,7 +78,7 @@ def change_password():
 @jwt_required()
 @catch_http_errors
 @spec.validate(
-     cookies=Cookies, tags=["users"]
+    cookies=Cookies, tags=["users"]
 )
 def logout_all_devices():
     """
@@ -99,7 +101,7 @@ def logout_all_devices():
 @routes.get('account/logout')
 @jwt_required()
 @spec.validate(
-     cookies=Cookies, tags=["users"]
+    cookies=Cookies, tags=["users"]
 )
 def logout():
     """
@@ -146,7 +148,7 @@ def login():
 @jwt_required(refresh=True)
 @custom_jwt_required()
 @spec.validate(
-     cookies=Cookies, tags=["users"]
+    cookies=Cookies, tags=["users"]
 )
 def refresh() -> Response:
     """обновляем access_token"""
@@ -158,9 +160,10 @@ def refresh() -> Response:
 @routes.get('/<string:login>/roles')
 @catch_http_errors
 @custom_jwt_required(admin_only=True)
-@spec.validate(query=QueryLogin, cookies=Cookies, tags=["users"],
-    cookies=Cookies, path_parameter_descriptions={'loging':'это логин'}, tags=["users"]
-)
+@spec.validate(query=QueryLogin, cookies=Cookies,
+               path_parameter_descriptions={'loging': 'это логин'},
+               tags=["users"]
+               )
 def get_user_roles(login: str) -> dict:
     roles = db_manager.roles.get_user_roles_by_login(
         login
@@ -172,8 +175,8 @@ def get_user_roles(login: str) -> dict:
 @custom_jwt_required(this_user_only=True)
 @catch_http_errors
 @spec.validate(query=QueryLogin, cookies=Cookies, tags=["users"],
-   cookies=Cookies, tags=["users"]
-)
+               cookies=Cookies, tags=["users"]
+               )
 def get_user_session(login: str) -> dict:
     sessions = db_manager.users.get_user_sessions(
         login
