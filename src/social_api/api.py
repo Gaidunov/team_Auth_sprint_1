a1056aff32e22d32d149c14b5e34a61dc670b84b
kbondar17 <ironbondar@gmail.com>
2022-11-22 17:35:50 +0300

    fix social api

--- src/social_api/api.py
+++ src/social_api/api.py
@@ -2,10 +2,10 @@ from src.social_api.vk import vk_api
 
 
 class SocialApi:
-    """это интерфейс, в котором собираются апи всех сервисов"""
+    """интерфейс, в котором собираются апи всех сервисов"""
 
     def __init__(self) -> None:
         self.vk = vk_api
-
+ 
 
 social_api = SocialApi()
--- src/social_api/vk.py
+++ src/social_api/vk.py
@@ -1,34 +1,25 @@
 import requests
- 
+
 import backoff
 from pydantic.error_wrappers import ValidationError
 
 from src.social_api.models import VkAccessResponse
 from src.config import vk_settings
 
-
-class VKValidationError(Exception):
-    pass
-
-
 class VkApi:
-    @backoff.on_exception(
-        backoff.expo,
-        requests.exceptions.RequestException
-    )
-    def get(self, url):
-        return requests.get(url).content
-
-    def get_access_token(self, code: str) -> VkAccessResponse:
+ 
+    @backoff.on_exception(backoff.expo,
+                      requests.exceptions.RequestException)
+    def get_access_token(self, code:str)->VkAccessResponse: 
         url = 'https://oauth.vk.com/access_token'
         params = vk_settings.dict()
-        params.update({'code': code})
+        params.update({'code':code})    
         vk_data = requests.get(url, params=params).json()
         try:
             vk_data = VkAccessResponse(**vk_data)
             return vk_data
         except ValidationError:
-            raise VKValidationError('VK data validation error')
-
-
+            print('не получается залогиниться ВК')
+            
+        
 vk_api = VkApi()
